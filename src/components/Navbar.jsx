b5ab225b721e5ef02c26ba62eb256e9b735671ee
Leonardo Diaz <jldiaz9623@gmail.com>
2025-05-19 18:55:19 -0500

    Diseño navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <nav className="">
-      <Link to="/">Cinevibe-Voyage</Link>
-      <Link to="/">Home</Link>
-      <Link to="/saved">Saved Plans</Link>
+    <nav className="bg-black p-4 shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl font-bold text-red-600">
+          Cinematic Journey
+        </Link>
+        <div className="space-x-4">
+          <Link to="/" className="hover:text-red-600">Home</Link>
+          <Link to="/saved" className="hover:text-red-600">Saved Plans</Link>
+        </div>
+      </div>
     </nav>
   )
 }
