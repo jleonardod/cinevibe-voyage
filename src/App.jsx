8c034d805db8f1eacd5d164585d67194382e2822
Leonardo Diaz <leonardo.diaz@periferia-it.pe>
2025-05-19 18:55:19 -0500

    Diseño navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router } from "react-router-dom"
 
 function App() {
  return (
-    <div className='bg-red-500'>
-      Hola mundo
-    </div>
+    <Router>
+      <Navbar />
+      Hola
+    </Router>
   )
 }
 
