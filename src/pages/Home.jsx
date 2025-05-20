0f4b365a7447d4adedd66754555625b1ca54f766
Leonardo Diaz <jldiaz9623@gmail.com>
2025-05-20 10:12:15 -0500

    Inclusion componente Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Home() {
+  return(
+    <div className="container mx-auto p-4 pt-20">
+      <h1 className="text-4xl font-bold text-center mb-8 text-red-600">CineVibe Voyage</h1>
+      <p className="text-center mb-8 max-w-2xl mx-auto">
+        Crea una noche de cine perfecta con CineVibe Voyage. Elige tu estado de ánimo y género y te recomendaremos películas, comida y una actividad para disfrutar.
+      </p>
+    </div>
+  )
+}
+
+export default Home
