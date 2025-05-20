198ca8821e50702c1e9e78ba6817d441a3f78436
Leonardo Diaz <leonardo.diaz@periferia-it.pe>
2025-05-20 10:12:15 -0500

    Inclusion componente Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar"
-import { BrowserRouter as Router } from "react-router-dom"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
  return (
     <Router>
       <div className="min-h-screen bg-gray-900 text-white">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   )
